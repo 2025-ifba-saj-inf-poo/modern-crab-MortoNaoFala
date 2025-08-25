ab2362e3ace1cc1d60ae3cec563feb82ab8160b8
JessicaOTeixeira <jessica.teixeira98@hotmail.com>
2025-08-25 11:37:50 -0300

    FIm Decapoda

---
+++ Lobster.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Lobster here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lobster extends Decapoda
+{
+    public Lobster(){        
+        super("a","d",550,550);        
+    }    
+}
